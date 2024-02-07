0ac90cd8947a404d86c28cd2d4a1bdab6dc4ee9b
Yadudev <vyadudev@gmail.com>
2024-02-08 03:09:43 +0530

    Home page created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products = [
+    {
+      name:"IPHONE 14",
+      category:"Mobile",
+      description:"launched in September 2021",
+      image:"https://idestiny.in/wp-content/uploads/2022/09/iPhone_14_Pro_Deep_Purple_PDP_Image_Position-1a_Avail__en-IN.jpg"
+    },
+    {
+      name:"One Plus 7T",
+      category:"Mobile",
+      description:"launched in September 2021",
+      image:"https://media.mobex.in/media/catalog/product/o/n/oneplus_7t_frosted_silver_8gb_ram.jpg?auto=webp&format=pjpg&width=640&height=800&fit=cover"
+    },
+    {
+      name:"Samasung S22",
+      category:"Mobile",
+      description:"launched in September 2021",
+      image:"https://m.media-amazon.com/images/I/71wGLBDEsvL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+      name:"MI note 9 pro",
+      category:"Mobile",
+      description:"launched in September 2021",
+      image:"https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1583932998.16321918!400x400!85.png"
+    },
+  ]
+  res.render('index', { products });
 });
 
 module.exports = router;
